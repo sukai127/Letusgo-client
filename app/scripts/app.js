e4ea4a3299020bd5efbbd6de5de1f7d6ddb58bab
SnakerWorker <snakerworker@gmail.com>
2014-09-15 21:27:50 +0800

    modify app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,23 +1,60 @@
 'use strict';
 
+/**
+ * @ngdoc overview
+ * @name angularLetusgoApp
+ * @description
+ * # angularLetusgoApp
+ *
+ * Main module of the application.
+ */
 angular
-  .module('letusgoApp', [
+  .module('angularLetusgoApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'LocalStorageModule'
   ])
+
+  .config(function (localStorageServiceProvider) {
+    localStorageServiceProvider.setPrefix('ls');
+  })
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
         controller: 'MainCtrl'
       })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
+      .when('/list/:pageNow', {
+        templateUrl: 'views/list.html',
+        controller: 'ListCtrl'
+      })
+      .when('/categoryManage', {
+        templateUrl: 'views/categoryManage.html',
+        controller: 'CategoryManageCtrl'
+      })
+      .when('/productManage', {
+        templateUrl: 'views/productManage.html',
+        controller: 'ProductManageCtrl'
+      })
+      .when('/updateCategory/:id', {
+        templateUrl: 'views/updateCategory.html',
+        controller: 'CategoryManageCtrl'
+      })
+      .when('/updateProduct/:name', {
+        templateUrl: 'views/updateProduct.html',
+        controller: 'UpdateProductCtrl'
+      })
+      .when('/result', {
+        templateUrl: 'views/result.html',
+        controller: 'ResultCtrl'
+      })
+      .when('/cart', {
+        templateUrl: 'views/cart.html',
+        controller: 'CartCtrl'
       })
       .otherwise({
         redirectTo: '/'
