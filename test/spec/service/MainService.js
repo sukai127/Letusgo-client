484dbdcb03297882eb8128d96d16c62f83071ae1
SnakerWorker <snakerworker@gmail.com>
2014-09-18 10:11:40 +0800

    add test initProduct

--- test/spec/service/MainService.js
+++ test/spec/service/MainService.js
@@ -1,3 +1,38 @@
-/**
- * Created by sukai on 9/18/14.
- */
+
+'use strict';
+
+describe('Service: ProductService', function () {
+
+  var localStorageService, mainService, products,categories;
+  beforeEach(function () {
+    module('letusgo');
+    inject(function ($injector) {
+      localStorageService = $injector.get('localStorageService');
+      mainService = $injector.get('MainService');
+    });
+    products = [
+      {name: 'Instant_noodles', unit: 'bag', category: '1', price: 1},
+      {name: 'apple', unit: 'kg', category: '1', price: 2.5}
+    ];
+    categories = [
+      {id : 1, name: 'grocery'},
+      {id : 2, name: 'device'}
+    ];
+  });
+
+  it('should initProducts() be worked when localStorage not empty', function () {
+    spyOn(localStorageService, 'get').and.returnValue(products);
+    spyOn(localStorageService,'add');
+    mainService.initProducts();
+    expect(localStorageService.add.calls.count()).toEqual(0);
+  });
+
+  it('should initProducts() be worked when localStorage is empty', function () {
+    spyOn(localStorageService, 'get').and.returnValue(null);
+    spyOn(localStorageService,'add');
+    mainService.initProducts();
+    expect(localStorageService.add.calls.count()).toEqual(1);
+  });
+
+});
+
