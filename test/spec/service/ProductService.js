b055f19ca1ba1c39c3dd4cabde77ec68bcf65bd4
SnakerWorker <snakerworker@gmail.com>
2014-09-18 10:47:32 +0800

    add test getPageTotal

--- test/spec/service/ProductService.js
+++ test/spec/service/ProductService.js
@@ -19,17 +19,38 @@ describe('Service: ProductService', function () {
     ];
   });
 
-  it('should loadAllProducts() work', function () {
+  it('should loadAllProducts() work  when pageNow not null', function () {
     spyOn(localStorageService, 'get').and.returnValue(products);
     var result = productService.loadAllProducts(1);
     expect(result.length).toEqual(2);
     expect(result[1].name).toBe('apple');
   });
 
-  it('should getPageTotal() work', function () {
+  it('should loadAllProducts() work  when pageNow is null', function () {
+    spyOn(localStorageService, 'get').and.returnValue(products);
+    var result = productService.loadAllProducts(null);
+    expect(result.length).toEqual(2);
+    expect(result[1].name).toBe('apple');
+  });
+
+  it('should loadAllProducts() work  when localStorage is empty', function () {
+    spyOn(localStorageService, 'get').and.returnValue(null);
+    var result = productService.loadAllProducts(null);
+    expect(result.length).toEqual(0);
+  });
+
+  it('should getPageTotal() work when product.length % 2 == 0', function () {
     spyOn(localStorageService, 'get').and.returnValue(products);
     var result = productService.getPageTotal();
     expect(result.length).toEqual(1);
+    expect(result[0]).toBe(1);
+  });
+
+  it('should getPageTotal() work when product.length % 2 != 0', function () {
+    products[2] = {name: 'banana', unit: 'kg', category: '1', price: 5.5};
+    spyOn(localStorageService, 'get').and.returnValue(products);
+    var result = productService.getPageTotal();
+    expect(result.length).toEqual(2);
     expect(result[0]).toBe(1);
   });
 
