90b43ed490badbaedbaa6c511e558ee210d3d589
SnakerWorker <snakerworker@gmail.com>
2014-09-20 17:09:28 +0800

    modify all method with

--- app/scripts/service/CategoryManageService.js
+++ app/scripts/service/CategoryManageService.js
@@ -1,45 +1,56 @@
 'use strict';
 angular.module('letusgo')
-    .service('CategoryManageService',function(localStorageService){
-        this.loadAllCategories = function(){
-            return localStorageService.get('categories');
+    .service('CategoryManageService',function(localStorageService,$http){
+        this.loadAllCategories = function(callback){
+          $http.get('/api/categories').success(function(data){
+            callback(data);
+          });
         };
         this.add = function(categories){
-          localStorageService.add('categories',categories);
+          $http({
+            url: '/api/categories',
+            method: 'POST',
+            params:{categories:categories}
+          });
           return categories;
         };
-        this.insert = function(name){
-          var categories = this.loadAllCategories();
-          var isExist = _.some(categories,{name : name});
-          if(name && !isExist){
-            var id = parseInt(categories[categories.length-1].id) + 1;
-            var category = {id: id,name : name};
-            categories.push(category);
-          }
-          return categories;
+        this.insert = function(name, callback){
+          this.loadAllCategories(function(categories){
+            var isExist = _.some(categories,{name : name});
+            if(name && !isExist){
+              var id = parseInt(categories[categories.length-1].id) + 1;
+              var category = {id: id,name : name};
+              categories.push(category);
+            }
+            callback(categories);
+          });
         };
 
-        this.isIncludeProduct = function(id){
-          var products = localStorageService.get('products');
-          var result = _.find(products,function(product){
-            return product.category === ''+id;
+        this.isIncludeProduct = function(id,callback){
+          $http.get('/api/categories').success(function(products){
+            var result = _.find(products,function(product){
+              return product.category === ''+id;
+            });
+            callback(result);
           });
-          return result ? true : false;
         };
-        this.getCategoryById = function(id){
-            var categories = localStorageService.get('categories');
-            return _.find(categories,function(category){
+        this.getCategoryById = function(id,callback){
+          $http.get('/api/categories').success(function(categories){
+            var result =  _.find(categories,function(category){
               return category.id+'' === ''+id;
             });
+            callback(result);
+          });
         };
         this.updateCategory = function(category){
-          var categories = localStorageService.get('categories');
-          _.forEach(categories,function(item,index){
-            if(item.id === category.id){
-              categories[index] = category;
-            }
+          $http.get('/api/categories').success(function(categories){
+            _.forEach(categories,function(item,index){
+              if(item.id === category.id){
+                categories[index] = category;
+              }
+            });
+            $http.post('api/categories',categories);
           });
-          this.add(categories);
           return category;
         };
     });
