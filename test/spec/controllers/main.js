2069ebdd310e95ffb91ba3dd734c5c83cfb673d7
SnakerWorker <snakerworker@gmail.com>
2014-09-28 16:04:56 +0800

    modify test init

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -4,25 +4,21 @@ describe('Controller: MainCtrl', function () {
 
   beforeEach(module('letusgo'));
 
-  var createController,$controller,$scope,mainService;
+  var createController,$controller,$scope;
 
   beforeEach(inject(function ($injector) {
     $scope = $injector.get('$rootScope').$new();
     $controller = $injector.get('$controller');
-    mainService = $injector.get('MainService');
     createController = function(){
       return $controller('MainCtrl', {
-        $scope: $scope,
-        $MainService : mainService
+        $scope: $scope
       });
     };
   }));
 
   it('should init success', function () {
     spyOn($scope,'$emit');
-    spyOn(mainService,'init');
     createController();
     expect($scope.$emit).toHaveBeenCalled();
-    expect(mainService.init.calls.count()).toBe(1);
   });
 });
