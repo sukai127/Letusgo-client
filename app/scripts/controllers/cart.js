421c32ec68a129b374cd42ae2b91f795842c011e
SnakerWorker <snakerworker@gmail.com>
2014-09-28 18:33:13 +0800

    modify get method

--- app/scripts/controllers/cart.js
+++ app/scripts/controllers/cart.js
@@ -2,11 +2,15 @@
 
 angular.module('letusgo')
     .controller('CartCtrl', function ($scope,CartService) {
+
         CartService.get(function(data){
+
           $scope.cart = data;
           $scope.isCartEmpty = $scope.cart.cartItems.length === 0;
-          $scope.totalMoney = CartService.getTotalMoney($scope.cart);
+          $scope.totalMoney = CartService.getTotalMoney($scope.cart.cartItems);
+
         });
+    
         $scope.$emit('highLightActive','cart');
 
         $scope.getSubtotal = function(cartitem){
