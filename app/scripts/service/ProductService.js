399df1e68c0b59819ccf011cdbce3156ea3dcebd
SnakerWorker <snakerworker@gmail.com>
2014-09-28 17:06:08 +0800

    format productService.js

--- app/scripts/service/ProductService.js
+++ app/scripts/service/ProductService.js
@@ -4,24 +4,33 @@ angular.module('letusgo')
     .service('ProductService',function(CartItemService,CategoryManageService,CartService,$http){
         this.loadAllProducts = function(pageNow,callback){
             $http.get('/api/products').success(function(products){
+
               if(pageNow){
                 products  = products.slice((pageNow-1)*2,pageNow*2);
               }
+
               _.forEach(products,function(product){
+
                 CategoryManageService.getCategoryById(product.categoryId,function(data){
                   product.category = data;
                 });
               });
+
               callback(products);
             });
         };
+    
         this.getPageTotal = function(callback){
+
           this.loadAllProducts(null,function(data){
+
             var totalCount =data.length;
             var pageCount = totalCount % 2 === 0 ?  parseInt(totalCount / 2) : parseInt(totalCount / 2) + 1;
+
             callback(_.range(1,pageCount + 1));
           });
         };
+
         this.addToCart = function(product,callback){
 
            $http.get('cartItems').success(function(cartItems){
