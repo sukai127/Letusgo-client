e3335626861f5045f7c813e09bd80aebba8bea03
SnakerWorker <snakerworker@gmail.com>
2014-09-29 20:35:06 +0800

    format productMange.js

--- app/scripts/controllers/productManage.js
+++ app/scripts/controllers/productManage.js
@@ -3,12 +3,16 @@
 angular.module('letusgo')
   .controller('ProductManageCtrl', function ($scope,ProductService,CategoryService,$routeParams) {
 
-    ProductService.loadAllProducts(function(data){
+    ProductService.loadAllProducts(null,function(data){
+
       $scope.products = data;
+
       $scope.product = _.find($scope.products,function(product){
+
         var name = $routeParams.name || 0;
         return product.name.toString() === name.toString();
       });
+
       if($scope.product){
         CategoryService.getCategoryById($scope.product.categoryId,function(data){
           $scope.product.category = data;
@@ -26,17 +30,22 @@ angular.module('letusgo')
     };
 
     $scope.updateProduct = function(){
+
       _.forEach($scope.products,function(product,index){
          if(product.id.toString() === $scope.product.id.toString()){
            $scope.products[index] = $scope.product;
          }
       });
+
       ProductService.updateProduct($scope.product);
     };
 
     $scope.$emit('highLightActive','product');
+
     $scope.add = function(){
+
       ProductService.insert($scope.product,function(data){
+        
         $scope.products.push(data);
         $scope.product = null;
       });
