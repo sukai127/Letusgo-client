213d771af2b206710bab301c87d75fa607a90bd7
SnakerWorker <snakerworker@gmail.com>
2014-09-29 19:57:06 +0800

    format CategoriesManageService

--- app/scripts/service/CategoryManageService.js
+++ app/scripts/service/CategoryManageService.js
@@ -1,21 +1,29 @@
 'use strict';
 angular.module('letusgo')
     .service('CategoryManageService',function($http){
+
         this.loadAllCategories = function(callback){
+
             var service = this;
+
             $http.get('/api/categories').success(function(categories){
+
               _.forEach(categories,function(category){
                 service.isIncludeProduct(category.id,function(data){
                   category.couldDelete = data ? false: true;
                 });
+
                 callback(categories);
               });
             });
         };
 
         this.insert = function(name,callback){
+
           if(name){
+
             var category = {name:name};
+
             $http.post('/api/categories',{category:category}).success(function(){
               category.couldDelete = true;
               callback(category);
@@ -28,18 +36,22 @@ angular.module('letusgo')
         };
 
         this.isIncludeProduct = function(id,callback){
+
           $http.get('/api/products').success(function(products){
+
             var result = _.find(products,function(product){
-              return product.categoryId === ''+id;
+              return product.categoryId.toString() === id.toString();
             });
             callback(result);
           });
         };
+
         this.getCategoryById = function(id,callback){
           $http.get('/api/categories/'+id).success(function(data){
             callback(data);
           });
         };
+    
         this.updateCategory = function(category){
           delete category.couldDelete;
           $http.put('/api/categories/'+category.id,{category:category});
