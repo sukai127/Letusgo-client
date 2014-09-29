8cd1fc45928ad40850bb52a6a85fd7c73e82b330
SnakerWorker <snakerworker@gmail.com>
2014-09-29 20:24:57 +0800

    remove CartItemService

--- app/scripts/service/CartItemService.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-angular.module('letusgo')
-    .service('CartItemService',function(){
-        this.create = function(product,count){
-            return {product : product, count : count};
-        };
-    });
