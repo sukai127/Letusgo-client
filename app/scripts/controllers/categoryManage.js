0839be5fa4d83f70339c8860bc1926d3791d05b0
SnakerWorker <snakerworker@gmail.com>
2014-09-29 20:22:09 +0800

    modify categoryMange.js

--- app/scripts/controllers/categoryManage.js
+++ app/scripts/controllers/categoryManage.js
@@ -1,18 +1,21 @@
 'use strict';
 
 angular.module('letusgo')
-  .controller('CategoryManageCtrl', function ($scope,CategoryManageService,$routeParams) {
+  .controller('CategoryManageCtrl', function ($scope,CategoryService,$routeParams) {
 
-      CategoryManageService.loadAllCategories(function(categories){
+    CategoryService.loadAllCategories(function(categories){
+
         $scope.categories = categories;
+
         $scope.category = _.find($scope.categories,function(category){
           var id = $routeParams.id || 0;
           return category.id.toString() === id.toString();
         });
+
       });
 
       $scope.add = function(){
-        CategoryManageService.insert($scope.name,function(data){
+        CategoryService.insert($scope.name,function(data){
           $scope.categories.push(data);
         });
       };
@@ -20,11 +23,11 @@ angular.module('letusgo')
       $scope.$emit('highLightActive','category');
 
       $scope.remove = function(index){
-        CategoryManageService.delete($scope.categories[index].id);
+        CategoryService.delete($scope.categories[index].id);
         $scope.categories.splice(index,1);
       };
 
       $scope.updateCategory = function(){
-        CategoryManageService.updateCategory($scope.category);
+        CategoryService.updateCategory($scope.category);
       };
   });
