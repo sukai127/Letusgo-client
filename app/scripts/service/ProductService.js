41f39aac6bf17557f03e16b575b8fd5e583c0709
SnakerWorker <snakerworker@gmail.com>
2014-09-29 20:26:58 +0800

    modify ProductService

--- app/scripts/service/ProductService.js
+++ app/scripts/service/ProductService.js
@@ -36,6 +36,48 @@ angular.module('letusgo')
           return previous;
         };
 
+        this.loadAllProducts = function (callback) {
+          $http.get('/api/products').success(function(data){
+            _.forEach(data,function(item){
+              if(!item.category){
+                CategoryManageService.getCategoryById(item.categoryId,function(category){
+                  item.category = category;
+                });
+              }
+            });
+            callback(data);
+          });
+        };
+
+        this.insert = function(product,callback){
+
+          var isAllFullIn = product && product.name && product.price && product.unit && product.categoryId;
+          if(isAllFullIn){
+            $http.post('/api/products',{product:product}).success(function(){
+              CategoryManageService.getCategoryById(product.categoryId,function(category){
+                product.category = category;
+                callback(product);
+              });
+            });
+          }
+        };
+
+        this.delete = function(id){
+          $http.delete('/api/products/'+id);
+        };
+
+        this.updateProduct = function (product) {
+          delete product.category;
+          $http.put('/api/products/'+product.id,{product:product});
+        };
+
+        this.getProductByName = function(name,callback){
+          $http.get('/api/products').success(function(products){
+            var result = _.find(products,{name: name}) || {};
+            callback(result);
+          });
+        };
+
         this.getNext = function(pageNow,pageTotal){
           var next = pageNow + 1 > _.max(pageTotal) ? _.max(pageTotal) : pageNow + 1;
           return next;
