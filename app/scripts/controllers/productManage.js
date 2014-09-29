14be068983b8a2f0b4f26887aaebbe0851b4b555
SnakerWorker <snakerworker@gmail.com>
2014-09-29 20:30:17 +0800

    rename productMangeService productService

--- app/scripts/controllers/productManage.js
+++ app/scripts/controllers/productManage.js
@@ -1,9 +1,9 @@
 'use strict';
 
 angular.module('letusgo')
-  .controller('ProductManageCtrl', function ($scope,ProductManageService,CategoryService,$routeParams) {
+  .controller('ProductManageCtrl', function ($scope,ProductService,CategoryService,$routeParams) {
 
-    ProductManageService.loadAllProducts(function(data){
+    ProductService.loadAllProducts(function(data){
       $scope.products = data;
       $scope.product = _.find($scope.products,function(product){
         var name = $routeParams.name || 0;
@@ -21,7 +21,7 @@ angular.module('letusgo')
     });
 
     $scope.remove = function(index){
-      ProductManageService.delete($scope.products[index].id);
+      ProductService.delete($scope.products[index].id);
       $scope.products.splice(index,1);
     };
 
@@ -31,12 +31,12 @@ angular.module('letusgo')
            $scope.products[index] = $scope.product;
          }
       });
-      ProductManageService.updateProduct($scope.product);
+      ProductService.updateProduct($scope.product);
     };
 
     $scope.$emit('highLightActive','product');
     $scope.add = function(){
-      ProductManageService.insert($scope.product,function(data){
+      ProductService.insert($scope.product,function(data){
         $scope.products.push(data);
         $scope.product = null;
       });
