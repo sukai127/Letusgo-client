52dfbd5ab8a5751767e74949e540c08b7c6823af
SnakerWorker <snakerworker@gmail.com>
2014-10-05 21:45:54 +0800

    add test get

--- test/spec/service/CartService.js
+++ test/spec/service/CartService.js
@@ -23,11 +23,6 @@ describe('Service: CartService', function () {
               id : 2,
               productId : 2,
               count : 3
-          },
-          {
-              id : 3,
-              productId: 3,
-              count : 1
           }
       ];
 
@@ -46,8 +41,21 @@ describe('Service: CartService', function () {
       $httpBackend.expectGET('/api/products').respond(200,products);
       service.buildCartItem(cartItems,function(data){
         expect(data[0].product.name).toEqual('Instant_noodles');
-        expect(data.length).toBe(3);
+        expect(data.length).toBe(2);
       });
       $httpBackend.flush();
+  });
+
+  it('should get() worked success', function () {
+
+    $httpBackend.expectGET('/api/cartItems').respond(200,cartItems);
+    $httpBackend.expectGET('/api/products').respond(200,products);
+
+    service.get(function(data){
+      expect(data.cartItems[0].product.name).toEqual('Instant_noodles');
+      expect(data.cartItems.length).toBe(2);
+      expect(data.count).toBe(7);
+    });
+    $httpBackend.flush();
   });
 });
