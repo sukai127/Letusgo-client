c2face250aab55ecd27da8737f8fe1bea80914c3
SnakerWorker <snakerworker@gmail.com>
2014-10-05 18:18:01 +0800

    format the code

--- app/scripts/service/CartService.js
+++ app/scripts/service/CartService.js
@@ -1,75 +1,75 @@
 'use strict';
 
 angular.module('letusgo')
-    .service('CartService',function($http){
+  .service('CartService', function ($http) {
 
-        this.get = function(callback){
+    this.get = function (callback) {
 
-            var service = this;
+      var service = this;
 
-            $http.get('/api/cartItems').success(function(data){
+      $http.get('/api/cartItems').success(function (data) {
 
-              service.buildCartItem(data,function(cartItems){
+        service.buildCartItem(data, function (cartItems) {
 
-                var cart = {cartItems: cartItems,
-                  count: service.getTotalCount(cartItems)
-                };
+          var cart = {cartItems: cartItems,
+            count: service.getTotalCount(cartItems)
+          };
 
-                callback(cart);
-              });
-            });
-        };
+          callback(cart);
+        });
+      });
+    };
 
-        this.buildCartItem= function(cartItems,callback){
+    this.buildCartItem = function (cartItems, callback) {
 
-          $http.get('/api/products').success(function(data){
-            _.forEach(cartItems,function(cartItem){
-              var product = _.find(data,function(product){
-                return product.id.toString() === cartItem.productId.toString();
-              });
-              cartItem.product = product;
-            });
-            callback(cartItems);
+      $http.get('/api/products').success(function (data) {
+        _.forEach(cartItems, function (cartItem) {
+          var product = _.find(data, function (product) {
+            return product.id.toString() === cartItem.productId.toString();
           });
-        };
+          cartItem.product = product;
+        });
+        callback(cartItems);
+      });
+    };
 
-        this.add = function(cartItem){
-            $http.post('/api/cartItems',{cartItem:cartItem});
-        };
+    this.add = function (cartItem) {
+      $http.post('/api/cartItems', {cartItem: cartItem});
+    };
 
-        this.getTotalMoney = function(cartItems){
-            var sum = 0;
-            _.forEach(cartItems,function(cartitem){
-               sum += cartitem.product.price * cartitem.count;
-            });
-            return sum;
-        };
+    this.getTotalMoney = function (cartItems) {
+      var sum = 0;
+      _.forEach(cartItems, function (cartitem) {
+        sum += cartitem.product.price * cartitem.count;
+      });
+      return sum;
+    };
 
-        this.getTotalCount = function(cartItems){
+    this.getTotalCount = function (cartItems) {
 
-          return _.reduce(_.pluck(cartItems, 'count'), function (count1, count2) {
-            return count1 + count2;
-          });
-        };
+      return _.reduce(_.pluck(cartItems, 'count'), function (count1, count2) {
+        return count1 + count2;
+      });
+    };
 
-        this.getSubtotal = function(cartitem){
-            return (cartitem.product.price * cartitem.count).toFixed(2);
-        };
+    this.getSubtotal = function (cartitem) {
+      return (cartitem.product.price * cartitem.count).toFixed(2);
+    };
 
-        this.delete = function(cartItem){
-            $http.delete('/api/cartItems/'+cartItem.id);
-        };
+    this.delete = function (cartItem) {
+      $http.delete('/api/cartItems/' + cartItem.id);
+    };
 
-        this.update = function(cartItem){
-            $http.put('/api/cartItems/'+cartItem.id,{cartItem:cartItem});
-        };
+    this.update = function (cartItem) {
+      $http.put('/api/cartItems/' + cartItem.id, {cartItem: cartItem});
+    };
 
-        this.remove = function(callback){
-            $http.post('/api/payment').success(function(data,status){
-              if(status === 200){
-                callback();
-              }
-            });
-        };
-    });
+    this.remove = function (callback) {
+      $http.post('/api/payment').success(function (data, status) {
+        if (status === 200) {
+          callback();
+        }
+      });
+    };
+  });
 
