9fe276571e30b253437c44dbcb24b8d16aea6440
SnakerWorker <snakerworker@gmail.com>
2014-10-06 11:57:07 +0800

    add test isIncludeProduct

--- test/spec/service/CategoryService.js
+++ test/spec/service/CategoryService.js
@@ -14,17 +14,19 @@ describe('Service: categoryService', function () {
           {id : 2, name: 'device'}
         ];
         products = [
-          {id : 1, name: 'Instant_noodles', unit: 'bag', categoryId: '1', price: 1},
-          {id : 2, name: 'apple', unit: 'kg', categoryId: '1', price: 2.5}
+          {id : 1, name: 'Instant_noodles', unit: 'bag', categoryId: 1, price: 1},
+          {id : 2, name: 'apple', unit: 'kg', categoryId: 1, price: 2.5}
         ];
     });
 
-//    it('should loadAllCategories() work  when get() is OK', function () {
-//        spyOn(localStorageService,'get').and.returnValue(categories);
-//        var result = categoryManageService.loadAllCategories();
-//        expect(result.length).toEqual(2);
-//        expect(result[1].name).toBe('device');
-//    });
+    it('should isIncludeProduct() worked', function () {
+
+      $httpBackend.expectGET('/api/products').respond(200,products);
+      categoryService.isIncludeProduct(1,function(data){
+        expect(data.name).toBe('Instant_noodles');
+      });
+      $httpBackend.flush();
+    });
 //
 //    it('should add() work', function () {
 //      spyOn(localStorageService,'add');
