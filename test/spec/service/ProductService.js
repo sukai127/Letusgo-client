445fc0b3c7dc8192e0e13b36e54cd01c712d6c1b
SnakerWorker <snakerworker@gmail.com>
2014-10-06 23:01:43 +0800

    add test getPagetTotal

--- test/spec/service/ProductService.js
+++ test/spec/service/ProductService.js
@@ -55,20 +55,16 @@ describe('Service: ProductService', function () {
     });
     $httpBackend.flush();
   });
-//
-//  it('should loadAllProducts() work  when localStorage is empty', function () {
-//    spyOn(localStorageService, 'get').and.returnValue(null);
-//    var result = productService.loadAllProducts(null);
-//    expect(result.length).toEqual(0);
-//  });
-//
-//  it('should getPageTotal() work when product.length % 2 == 0', function () {
-//    spyOn(localStorageService, 'get').and.returnValue(products);
-//    var result = productService.getPageTotal();
-//    expect(result.length).toEqual(1);
-//    expect(result[0]).toBe(1);
-//  });
-//
+
+  it('should getPageTotal() work when product.length % 2 == 0', function () {
+    spyOn(productService,'loadAllProducts').and.callFake(function(pageNow,callback){
+      callback(products);
+    });
+    productService.getPageTotal(function(data){
+      expect(data.length).toBe(2);
+    });
+  });
+
 //  it('should getPageTotal() work when product.length % 2 != 0', function () {
 //    products[2] = {name: 'banana', unit: 'kg', category: '1', price: 5.5};
 //    spyOn(localStorageService, 'get').and.returnValue(products);
